663de0f083728dfa8ee7d1edaca550b2e925e5a1
ibrahimayuba393 <ibrahimayuba393@gmail.com>
2022-10-01 18:01:52 +0100

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   darkMode: "class",
   theme: {
     fontFamily: {
